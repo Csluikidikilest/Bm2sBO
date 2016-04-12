a05a6e9f2399616ed07b7f85d433b163e91d64db
Csluikidikilest <csluikidikilest@gmail.com>
2016-04-12 20:00:10 +0200

    Custom actions for datatables

--- Bm2sBO/Scripts/Directives/Datatable/datatable.js
+++ Bm2sBO/Scripts/Directives/Datatable/datatable.js
@@ -2,55 +2,59 @@
   function link($scope, element, attrs) {
     $scope.Math = window.Math;
 
+    $scope.action = function (event, line) {
+      $scope.onCustomAction({ event: event, line: line });
+    };
+
     $scope.add = function () {
       $scope.onAddItem();
-    }
+    };
 
     $scope.delete = function (line) {
       $scope.onDeleteItem({ line: line });
-    }
+    };
 
     $scope.edit = function (line) {
       $scope.onEditItem({ line: line });
-    }
+    };
 
     $scope.refresh = function () {
       $scope.onRefresh();
-    }
+    };
 
     $scope.$watch('CurrentPage', function (newValue, oldValue) {
-      if (newValue != oldValue) {
+      if (newValue !== oldValue) {
         $scope.refreshPageSize();
       }
     }, true);
 
     $scope.$watch('pageSize', function (newValue, oldValue) {
-      if (newValue != oldValue) {
+      if (newValue !== oldValue) {
         $scope.jumpToPage(0);
       }
     }, true);
 
     $scope.$watch('SearchFilter', function (newValue, oldValue) {
-      if (newValue != oldValue) {
+      if (newValue !== oldValue) {
         $scope.refreshPageSize();
       }
     }, true);
 
     $scope.$watch('source', function (newValue, oldValue) {
-      if (newValue) {
+      if (newValue !== oldValue) {
         $scope.ItemsCount = $scope.source.length;
         $scope.jumpToPage(0);
       }
     }, true);
 
     $scope.changeOrder = function (column) {
-      if ($scope.orderColumn == column) {
+      if ($scope.orderColumn === column) {
         $scope.orderReverse = !$scope.orderReverse;
       } else {
         $scope.orderColumn = column;
         $scope.orderReverse = false;
       }
-    }
+    };
 
     $scope.jumpToPage = function (currentPage) {
       $scope.CurrentPage = currentPage;
@@ -75,12 +79,14 @@
       canEdit: '=',
       columnsHeader: '=',
       currentPage: '=',
+      customActions: '=',
       entriesText: '=',
       interval: '=',
       largeStep: '=',
       loading: '=',
       ofText: '=',
       onAddItem: '&',
+      onCustomAction: '&',
       onDeleteItem: '&',
       onEditItem: '&',
       onRefresh: '&',
